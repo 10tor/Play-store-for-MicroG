9ce8af1513f9325ccb130dfa51b3e195e70d41d7
Tor <141473267+10tor@users.noreply.github.com>
2025-04-25 14:55:22 +0530

    Add files via upload

---
+++ customize.sh
@@ -0,0 +1,5 @@
+#Print welcome message
+ui_print "- If you can't find the Play store App After Reboot"
+ui_print "Install The provided App File from the GitHub"
+ui_print "- This happens Because of we are replacing a System Application (MicroG Companion) that Comes with MicroG"
+ui_print "- By 10tor"
